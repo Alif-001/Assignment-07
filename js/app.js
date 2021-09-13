f706e176892fcc766d50141f8c42dc7034ba696d
MD. OMOR FARUK ALIF <alifh8282@gmali.com>
2021-09-13 14:06:44 +0600

    style fix

--- js/app.js
+++ js/app.js
@@ -12,17 +12,19 @@ const showProducts = (products) => {
   for (const product of allProducts) {
     const image = product.image;
     const div = document.createElement("div");
-    div.classList.add("product");
+    div.classList.add("product", "m-5", "py-3");
     div.innerHTML = `
-    <div class="single-product">
+    <div class="single-product m-1 py-5 ">
       <div>
         <img class="product-image" src=${image}></img>
       </div>
-        <h3>${product.title}</h3>
-        <p>Category: ${product.category}</p>
-       <h2>Price: $ ${product.price}</h2>
-        <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-        <button id="details-btn" class="btn btn-danger">Details</button>
+        <h3  class='fs-1'>${product.title}</h3>
+        <p class='fs-3'>Category : ${product.category}</p>
+        <p class="fs-3">Ratings : ( ${product.rating.rate} )</p>
+        <p class="fs-3">Reviews by : ${product.rating.count}</p>
+       <h2 class='mb-3'>Price : $ ${product.price}</h2>
+        <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-outline-primary fs-4">add to cart</button>
+        <button id="details-btn" class="btn btn-outline-dark fs-4">Details</button>
     </div>`;
     document.getElementById("all-products").appendChild(div);
   }
@@ -38,7 +40,7 @@ const addToCart = (id, price) => {
 
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
-  const converted = parseInt(element);
+  const converted = parseFloat(element);
   return converted;
 };
 
@@ -47,12 +49,12 @@ const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+  document.getElementById(id).innerText = parseFloat(total).toFixed(2);
 };
 
 // set innerText function
 const setInnerText = (id, value) => {
-  document.getElementById(id).innerText = Math.round(value);
+  document.getElementById(id).innerText = parseFloat(value).toFixed(2);
 };
 
 // update delivery charge and total Tax
